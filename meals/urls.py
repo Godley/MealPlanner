1998c196c381a6cf337ce86ed07519474cd76cb6
Godley <charlotte.godley@hotmail.co.uk>
2017-03-15 21:24:55 +0000

    updated recipe view
    - added form to detail view template
    - added post view which calculates how much stock to remove

--- meals/urls.py
+++ meals/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^$', views.RecipeView.as_view(), name='index'),
     url(r'^menu/$', views.TwoWeeksRecipes.as_view(), name='menu'),
     url(r'^menu/ingredients/$', views.TwoWeeksFood.as_view(), name='shopping'),
+    url(r'^cooked/$', views.Cooked.as_view(), name='cooked'),
     url(r'^(?P<recipe_id>[0-9]+)/$', views.DetailView.as_view(), name="recipe")
 ]
--- meals/views.py
+++ meals/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse
 from django.views import View
-from meals.models import Recipe, Category
+from meals.models import Recipe, Category, Stock, StockItem, Ingredient
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
+
+def convert_units(stock_units, ingredient_units, value):
+    table = {"tsp": {"g": lambda x: x*5},
+             "tbsp": {"g": lambda x: x*15,
+                      "tsp": lambda x: x*3,
+                      "ml": lambda x: x*15,},
+             "cup": {"g": lambda x: x*200,
+                     "kg": lambda x: x*0.2},
+             "pound": {"g": lambda x: x*453}}
+    return table[ingredient_units][stock_units](value)
 
 # Create your views here.
 class RecipeView(View):
@@ -14,7 +25,40 @@ class DetailView(View):
     def get(self, request, recipe_id):
         recipe = Recipe.objects.filter(pk=recipe_id)[0]
         context = {'recipe': recipe}
+
+        ingredient_ids = {ingredient.item.pk: (ingredient.quantity, str(ingredient.units)) for ingredient in recipe.ingredient_set.all()}
+        request.session['ingredients'] = ingredient_ids
+        request.session['recipe'] = recipe_id
         return render(request, 'meals/detail.html', context)
+
+class Cooked(View):
+    def post(self, request):
+        ingredients = request.session['ingredients']
+        recipe = request.session['recipe']
+        portions = request.POST['portions']
+        results = []
+        recipe = Recipe.objects.get(pk=recipe)
+        multiplier = int(portions) / recipe.portions
+        for item in ingredients:
+            stockItem = StockItem.objects.get(pk=item)
+            try:
+                elem = Stock.objects.get(item=stockItem)
+
+                if ingredients[item][1] == str(elem.units):
+                    new_quantity = elem.quantity - (ingredients[item][0] * multiplier)
+                    results.append(str(new_quantity)+elem.item.name)
+
+                else:
+                    new_quantity = convert_units(str(elem.units), ingredients[item][1], ingredients[item][0])
+                    new_quantity = elem.quantity - (new_quantity * multiplier)
+                    results.append(str(new_quantity)+elem.item.name)
+
+                elem.quantity = new_quantity
+                elem.save()
+            except ObjectDoesNotExist:
+                print("not in stock, continuing")
+
+        return HttpResponse(content="Updated stock successfully")
 
 class TwoWeeksRecipes(View):
     def get(self, request):
